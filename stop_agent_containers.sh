f3d5c66cd979c616d8cd9c751014cebeaf152b9d
Ken Mugrage <kmugrage@gmail.com>
2014-05-18 18:38:21 -0700

    put the stop and rm commands in functions

--- stop_agent_containers.sh
+++ stop_agent_containers.sh
@@ -5,5 +5,18 @@
 #because I want them gone. If they aren't there it will just be
 #faster
 
-docker stop `docker ps --no-trunc -a -q` 2>/dev/null
-docker rm `docker ps --no-trunc -a -q` 2>/dev/null
+
+stop_containers()
+{
+	docker stop `docker ps --no-trunc -a -q` 2>/dev/null
+	return 1
+}
+
+rm_containers()
+{
+	docker rm `docker ps --no-trunc -a -q` 2>/dev/null
+	return 1
+}
+
+stop_containers
+rm_containers
